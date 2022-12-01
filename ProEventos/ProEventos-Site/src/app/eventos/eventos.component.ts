21056a3ea6f102cf892e0a8c58d01feb2afc57e6
Herbert Carvalho <betoo.carvalho.167@gmail.com>
2022-12-01 17:24:37 -0300

    Seção 5 Angular, Interpolação, Diretivas e Binding

--- ProEventos/ProEventos-Site/src/app/eventos/eventos.component.ts
+++ ProEventos/ProEventos-Site/src/app/eventos/eventos.component.ts
@@ -9,6 +9,29 @@ import { Component } from '@angular/core';
 export class EventosComponent {
 
   public eventos: any;
+  public eventosFiltrados: any = [];
+
+  widthImg: number = 150;
+  marginImg: number = 2;
+  exibirImg: boolean = true;
+  // ttlBtn: string = "Ocultar";
+  private _filtroLista: string = '';
+
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { };
 
@@ -18,9 +41,18 @@ export class EventosComponent {
 
   getEventos(){
     this.http.get('http://localhost:5000/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error),
-    );
+      );
+  }
+
+  exibirImagem() {
+    this.exibirImg = !this.exibirImg;
+    // if(!this.mostrarImg) this.ttlBtn = "Mostrar";
+    // else this.ttlBtn = "Ocultar";
   }
 
 }
